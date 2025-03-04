1fcd48914a2a25898686e7e41c7e5db623776b59
Shrine101 <lawrencesuitor@gmail.com>
2025-03-04 15:42:18 -0500

    dummy data

--- main.py
+++ main.py
@@ -6,4 +6,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    dummy_data = [
+        {
+            "id": 1,
+            "starting_time": "16:00",
+            "team_a": "Random Team 1",
+            "score": "0 - 0",
+            "team_b": "Random Team 2",
+            "minute": "00:00",
+        },
+        {
+            "id": 2,
+            "starting_time": "18:00",
+            "team_a": "Random Team 3",
+            "score": "0 - 0",
+            "team_b": "Random Team 4",
+            "minute": "00:00",
+        },
+    ]
+    return render_template(
+        'index.html',
+        matches=dummy_data
+        
+    )
