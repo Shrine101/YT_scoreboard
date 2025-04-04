1f610c9a4b10e438ce5ecc43b247d98a91b7d1ad
Shathurshika Chandrakumar <74083424+shath1955@users.noreply.github.com>
2025-04-04 14:01:00 -0400

    Update LED_testing.py

--- LED_testing.py
+++ LED_testing.py
@@ -1,31 +1,173 @@
+
+
+"""
+LEDs.py 
+
+Function: 
+This file is holds the functions to control the LED strips for the dartboard. 
+"""
+
 import time
-from LEDs import LEDs  # Import the LEDs class
+from rpi_ws281x import *
+import argparse
 
-def color_wipe_test():
-    leds = LEDs()  # Initialize the LED controller
-    color = (255, 0, 0)  # Red color for testing
+class LEDs:
 
-    print("Starting Color Wipe Test...")
+    def __init__(self):
+        # LED strip configuration:
+        self.NUM_STRIPS = 20
+        self.NUM_LED_PER_STRIP = 18
+        self.LED_COUNT      = self.NUM_STRIPS*self.NUM_LED_PER_STRIP + 3     # Number of LED pixels per strip
 
-    # Wipe each strip in sequence
-    for strip_num in range(21):
-        leds.colorWipe(strip_num, color, wait_ms=15)  # Run color wipe on each strip
-        time.sleep(0.1)  # Small delay between strips
-        leds.colorWipe(strip_num, (0,0,0), wait_ms=15)  # Run color wipe on each strip
+        self.LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+        self.LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+        self.LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+        self.LED_BRIGHTNESS = 240     # Set to 0 for darkest and 255 for brightest
+        self.LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+        self.LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-    # Check double & triple seg
-    for i in range(20): 
-        leds.tripleSeg(i, color)
-        leds.doubleSeg(i, color)
+        self.TRPL_RING = 9
+        self.DBL_RING = 0
+
+        # Initialize the LED strip
+        self.strip = Adafruit_NeoPixel(
+            self.LED_COUNT, self.LED_PIN, self.LED_FREQ_HZ,
+            self.LED_DMA, self.LED_INVERT, self.LED_BRIGHTNESS, self.LED_CHANNEL
+        )
+        self.strip.begin()
+
+    def getSegIndexes(self, strip_num):
     
-    leds.inner_bullseye(color)
-    leds.outer_bullseye(color)
+        start_seg = strip_num*self.NUM_LED_PER_STRIP
+        end_seg = start_seg + self.NUM_LED_PER_STRIP
+        
+        return start_seg, end_seg 
+    
+    # Sweep colors across strip     
+    def colorWipe(self, strip_num, color, wait_ms=50):
+        start_seg, end_seg = self.getSegIndexes(strip_num) 
+        for i in range(start_seg, end_seg):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+            time.sleep(wait_ms/1000.0)
 
+    # Turn off all LEDs
+    def clearAll(self, wait_ms=1): 
+        for i in range(self.strip.numPixels()):
+            self.strip.setPixelColor(i, Color(0,0,0))
+            self.strip.show()
+            time.sleep(wait_ms/1000.0)
 
-    time.sleep(0.5)
+    # Lights up number segment on outer circumference of dartboard 
+    def numSeg(self, strip_num, color, wait_ms=5):
+        # light up number segment outside of dartboard
+        if(strip_num % 2 == 0): #even num strip 
+            pixel = self.NUM_RING + self.NUM_LED_PER_STRIP*strip_num
+        else: # odd num strip 
+            pixel = self.NUM_LED_PER_STRIP*strip_num + (self.NUM_LED_PER_STRIP - 1)
+            
+        self.strip.setPixelColor(pixel, color)
+        self.strip.show()
+        time.sleep(wait_ms/1000.0)
+        
+    # Lights up triple segment 
+    def tripleSeg(self, strip_num, color, wait_ms=5):
+        # light up triple segment
+        if(strip_num % 2 == 0): #even num strip 
+            pixel = self.TRPL_RING + self.NUM_LED_PER_STRIP*strip_num
+        else: # odd num strip 
+            pixel = self.NUM_LED_PER_STRIP*strip_num + (self.NUM_LED_PER_STRIP - self.TRPL_RING - 1)
+        
+        self.strip.setPixelColor(pixel, color)
+        self.strip.show()
+        time.sleep(wait_ms/1000.0)
+        
+    # Lights up double segment 
+    def doubleSeg(self, strip_num, color, wait_ms=5):
+        # light up triple segment
+        if(strip_num % 2 == 0): #even num strip 
+            pixel = self.DBL_RING + self.NUM_LED_PER_STRIP*strip_num
+        else: # odd num strip 
+            pixel = self.NUM_LED_PER_STRIP*strip_num + (self.NUM_LED_PER_STRIP - self.DBL_RING - 1)
+        
+        self.strip.setPixelColor(pixel, color)
+        self.strip.show()
+        time.sleep(wait_ms/1000.0)
 
-    leds.clearAll()
+    # Lights up outer single segment (closest to circumference)
+    def outerSingleSeg(self, strip_num, color, wait_ms=5):
+        
+        if(strip_num % 2 == 0): #even num strip 
+            start_seg = (self.DBL_RING + self.NUM_LED_PER_STRIP*strip_num) + 1
+            end_seg = self.TRPL_RING + self.NUM_LED_PER_STRIP*strip_num
+        else: # odd num strip
+            end_seg = self.NUM_LED_PER_STRIP*strip_num + (self.NUM_LED_PER_STRIP - self.DBL_RING) - 1
+            start_seg = self.NUM_LED_PER_STRIP*strip_num + (self.NUM_LED_PER_STRIP - self.TRPL_RING)
+        
+        
+        for i in range(start_seg, end_seg):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+            time.sleep(wait_ms/1000.0)
 
+    # Lights up inner single segment (furthest from circumference)
+    def innerSingleSeg(self, strip_num, color, wait_ms=5):
+        
+        if(strip_num % 2 == 0): #even num strip 
+            start_seg = self.NUM_LED_PER_STRIP*strip_num + self.TRPL_RING + 1 
+            end_seg = self.NUM_LED_PER_STRIP*strip_num + self.NUM_LED_PER_STRIP
+        else: # odd num strip
+            start_seg = self.NUM_LED_PER_STRIP*strip_num 
+            end_seg = self.NUM_LED_PER_STRIP*strip_num + (self.NUM_LED_PER_STRIP - self.TRPL_RING - 1)
+        
+        
+        for i in range(start_seg, end_seg):
+            self.strip.setPixelColor(i, color)
+            self.strip.show()
+            time.sleep(wait_ms/1000.0)
+            
+    def inner_bullseye(self, color, wait_ms=5):
+        """Lights up the bullseye (centre LED)"""
+        bullseye_pixel = self.LED_COUNT - 1  # Assuming the last LED represents the bullseye
+        self.strip.setPixelColor(bullseye_pixel, color)
+        self.strip.show()
+        time.sleep(wait_ms / 1000.0)
+        
+    def outer_bullseye(self, color, wait_ms=5):
+        """Lights up the bullseye (centre LED)"""
+        bullseye_pixel_1 = self.LED_COUNT - 2  # Assuming the last LED represents the bullseye
+        self.strip.setPixelColor(bullseye_pixel_1, color)
+        self.strip.show()
+        time.sleep(wait_ms / 1000.0)
+        bullseye_pixel_2 = self.LED_COUNT - 3  # Assuming the last LED represents the bullseye
+        self.strip.setPixelColor(bullseye_pixel_2, color)
+        self.strip.show()
+        time.sleep(wait_ms / 1000.0)
 
-if __name__ == "__main__":
-    color_wipe_test()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--clear', action='store_true', help='Clear the display on exit')
+    args = parser.parse_args()
+
+    led_control = LEDs()
+
+    print("Press Ctrl-C to quit.")
+    if not args.clear:
+        print("Use '-c' argument to clear LEDs on exit")
+    
+    color = (200, 200, 200)  
+
+    try:
+       for i in range(20): 
+           led_control.tripleSeg(i, color)
+           led_control.doubleSeg(i, color)
+       led_control.inner_bullseye(color)
+       led_control.outer_bullseye(color)
+        
+    except KeyboardInterrupt:
+        if args.clear:
+            led_control.clearAll()
+        print("\nExiting program.")
+
+if __name__ == '__main__':
+    main()
