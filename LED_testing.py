a5afa6c7e1ac485fd5e8b8395d140ba778b35085
Shathurshika Chandrakumar <74083424+shath1955@users.noreply.github.com>
2025-04-04 14:13:01 -0400

    Update LED_testing.py

--- LED_testing.py
+++ LED_testing.py
@@ -1,28 +1,28 @@
 import time
- from LEDs import LEDs  # Import the LEDs class
+from LEDs import LEDs  # Import the LEDs class
  
- def color_wipe_test():
-     leds = LEDs()  # Initialize the LED controller
-     color = (255, 0, 0)  # Red color for testing
- 
-     print("Starting Color Wipe Test...")
- 
-     # Wipe each strip in sequence
-     # for strip_num in range(20):
-     #     leds.colorWipe(strip_num, color, wait_ms=15)  # Run color wipe on each strip
-     #     time.sleep(0.1)  # Small delay between strips
-     #     leds.colorWipe(strip_num, (0,0,0), wait_ms=15)  # Run color wipe on each strip
- 
-     # Check double & triple seg
-     for i in range(20): 
-         leds.tripleSeg(i, color)
-         leds.doubleSeg(i, color)
- 
-     leds.inner_bullseye(color)
-     leds.outer_bullseye(color)
+def color_wipe_test():
+    leds = LEDs()  # Initialize the LED controller
+    color = (255, 0, 0)  # Red color for testing
 
- 
-     time.sleep(0.5)
+    print("Starting Color Wipe Test...")
+
+    # Wipe each strip in sequence
+    # for strip_num in range(20):
+    #     leds.colorWipe(strip_num, color, wait_ms=15)  # Run color wipe on each strip
+    #     time.sleep(0.1)  # Small delay between strips
+    #     leds.colorWipe(strip_num, (0,0,0), wait_ms=15)  # Run color wipe on each strip
+
+    # Check double & triple seg
+    for i in range(20): 
+        leds.tripleSeg(i, color)
+        leds.doubleSeg(i, color)
+
+    leds.inner_bullseye(color)
+    leds.outer_bullseye(color)
+
+
+    time.sleep(0.5)
  
      leds.clearAll()
  
